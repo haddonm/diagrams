00a075c2e34f508dd49357ebdcdc4a8ceca8af06
Malcolm Haddon <malcolm.haddon@gmail.com>
2019-05-17 09:17:41 +1000

    First commit of all functions. No errors, no messages, and no notes.

---
+++ data-raw/use-diagrams.r
@@ -0,0 +1,26 @@
+
+library(rutilsMH)
+library(diagrams)
+
+plotprep(height=5)
+canvas()
+
+
+canvas()
+circle(origx=35,origy=70,radius=30,lwd=2)
+circle(origx=65,origy=60,radius=30,lwd=2,col=1)
+circle(origx=45,origy=40,radius=30,lwd=2,col=1)
+text(35,105,"Previous Year - p",cex=1.0,pos=1)
+text(70,95,"Current Year - c",cex=1.0,pos=1)
+text(45,10,"Next Year - n",cex=1.0,pos=1)
+
+
+
+
+
+
+
+
+
+
+
